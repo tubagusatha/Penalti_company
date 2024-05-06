3921c66ca92b4e342a6bb6316ae8626f253b6cfd
ones <almanafyounes@gmail.com>
2024-05-06 09:34:58 +0300

    profile_ver1

--- E-Commerce/resources/views/layout/auth.blade.php
+++ E-Commerce/resources/views/layout/auth.blade.php
@@ -58,6 +58,7 @@
   <body class="bg-gray-50 dark:bg-gray-800">
     @yield('main')
     <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script src="{{asset('js/sidebar.js')}}"></script>
 <script src="https://flowbite-admin-dashboard.vercel.app//app.bundle.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.2/datepicker.min.js"></script>
   </body>
--- E-Commerce/routes/web.php
+++ E-Commerce/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::get('/', function () {
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::post('/register', [AuthController::class, 'register']);
+Route::get('/profile', [ProfileController::class,'profile']);
 
 
 Route::get('/admin_panel', [AdminController::class,'index']);
