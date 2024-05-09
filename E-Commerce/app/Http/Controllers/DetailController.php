f718000343d1a033105ce2bb4054a204f4133adb
ones <almanafyounes@gmail.com>
2024-05-09 07:13:20 +0300

    detailandprofile

---
+++ E-Commerce/app/Http/Controllers/DetailController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DetailController extends Controller
+{
+    public function detail(){
+        return view('detail.index');
+    }
+    public function detail_tshirt(){
+        return view('detail.tshirt.index');
+    }
+    public function detail_shirt(){
+        return view('detail.shirt.index');
+    }
+    public function detail_pants(){
+        return view('detail.pants.index');
+    }
+    public function detail_accessories(){
+        return view('detail.accessories.index');
+    }
+
+}
---
+++ E-Commerce/resources/views/detail/tshirt/index.blade.php
@@ -0,0 +1,113 @@
+@extends('layout.app')
+@extends('layout.nav')
+
+@section('main')
+<div class="container max-w-full sm:max-w-full">
+        <!-- top profile -->
+        <div class="max-w-full flex bg-dark">
+            <div class="text-center py-10 px-5 items-center justify-center flex flex-wrap">
+            <div class=",">
+                <h1>Tshirt</h1>
+            </div>
+            <div class="text-white text-sm py-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, temporibus! Quod numquam dicta dolore assumenda et, obcaecati saepe ducimus cum.</p>
+            </div>
+            </div>
+            <div class="grid grid-cols-2 gap-2 md:grid-cols-4 md:gap-4 px-5">
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href="#"><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm xl:text-lg">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+                <div class="col-span-1">
+                    <div class="max-w-full px-2">
+                    <a href=""><img src="{{ asset('assets/img/pic_profile_order.svg') }}" alt="Pic" class="w-full h-auto"></a>
+                    <div class="py-2">
+                    <h1 class="text-white text-sm">Classic Polo Shirt</h1>
+                    <p class="text-white text-[9px]">Rp 128.000</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>
+
+
+@endsection
